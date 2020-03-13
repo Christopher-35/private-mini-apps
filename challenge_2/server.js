1d6763efdd6e67cdba2caa245e12505dfe7f5d37
Chris Roback <RobackChristopher@gmail.com>
2020-03-12 23:42:28 -0400

    complete setting up working server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,15 @@
-app.use('/static', express.static(path.join(__dirname, 'client')))
+const express = require('express');
+const app = express()
+const path = require('path');
+const port = 3000
 
+app.get('/', (req, res) => res.send('Heyzy World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.use('/static', express.static(path.join(__dirname, 'client')))
+
+//in pack.json
+  //don't need main value
+  //make sure you have express as a dependency
+  //have nodemon in npm start
+  ///*"node server.js"*/ nodemon --ignore node_modules server

03cc7f22af6a1215e8c2c617dab950807fdd1ad7
Chris Roback <RobackChristopher@gmail.com>
2020-03-17 21:18:32 -0400

    complete creating database schema

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,8 +1,28 @@
+//SERVER------------------------------------
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
 const morgan = require('morgan');
+//DATABASE---------------------------------------
+const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'sparkie1218',
+  database: 'userInfo'
+})
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
 
 
 
@@ -14,3 +34,7 @@ app.post('/', (req, res) => {
   res.redirect('/f1')
 })
  app.listen(port, () => console.log('server running...'));
+
+ module.exports = connection;
+
+ 

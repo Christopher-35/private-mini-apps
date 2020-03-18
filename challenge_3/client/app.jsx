ebc871edaed4865d8e1fdc1deac4d4ffdc9bdb8f
Chris Roback <RobackChristopher@gmail.com>
2020-03-17 23:06:45 -0400

    complete getting react to render properly

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,26 +1,68 @@
-var App = (props) => (
+
+// var App = (props) => (
 
 
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <div><h5><em>videoPlayer</em> view goes here</h5></div>
+//       </div>
+//       <div className="col-md-5">
+//       <VideoListEntry videos={this.props.videos} />
+//         <div><h5><em>videoList</em> view goes here</h5></div>
+//       </div>
+//     </div>
+//   </div>
+
+
+// );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    // this.handleChange = this.handleChange.bind(this);
+    // this.state = {
+    //   // "DataSource" is some global data source
+    //   comments: DataSource.getComments()
+    // };
+  }
+
+  // componentDidMount() {
+  //   // Subscribe to changes
+  //   DataSource.addChangeListener(this.handleChange);
+  // }
+
+  // componentWillUnmount() {
+  //   // Clean up listener
+  //   DataSource.removeChangeListener(this.handleChange);
+  // }
+
+  // handleChange() {
+  //   // Update component state whenever the data source changes
+  //   this.setState({
+  //     comments: DataSource.getComments()
+  //   });
+  // }
+
+  render() {
+    return (
+      <div>
+        {/* {this.state.comments.map((comment) => (
+          <Comment comment={comment} key={comment.id} />
+        ))} */}
+        dfsdzsgvvs
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-      <VideoListEntry videos={this.props.videos} />
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-
-
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-export default App;
+//export default App;
+
+ReactDOM.render(<App/>, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -11,7 +11,7 @@ let connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'sparkie1218',
-  database: 'userInfo'
+  database: 'userinfo'
 })
 
 connection.connect(function(err) {
@@ -30,11 +30,43 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use(morgan('short'));
 
+// app.get('/', (req, res) => {
+
+// })
+
 app.post('/', (req, res) => {
   res.redirect('/f1')
 })
+
+app.get('/f1', (req, res) => {
+
+})
+
+app.post('/f1', (req, res) => {
+  res.redirect('/f2')
+})
+
+// app.get('/f2', (req, res) => {
+
+// })
+
+app.post('/f2', (req, res) => {
+  res.redirect('/f3')
+})
+
+// app.get('/f3', (req, res) => {
+// })
+
+app.post('/f3', (req, res) => {
+  res.redirect('/final')
+})
+
  app.listen(port, () => console.log('server running...'));
+
+
+// ReactDOM.render(<App />, document.getElementById("app"));
+
+
 
  module.exports = connection;
 
- 
